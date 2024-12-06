3a0e96280cf483168f3fb5f9c9765a16445fe294
Richard Musime <richard.musime@moyotech.solutions>
2024-12-06 16:58:42 +0200

    feat: added the application logic

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,9 +1,20 @@
-sap.ui.define(["./BaseController", "sap/m/MessageBox"], function (BaseController, MessageBox) {
+sap.ui.define(["./BaseController", "sap/m/MessageBox",
+	"sap/ui/model/odata/v2/ODataModel"
+], function (BaseController, MessageBox,ODataModel) {
 	"use strict";
 
 	return BaseController.extend("moyotech.controller.Main", {
-		sayHello: function () {
-			MessageBox.show("Hello World!");
-		}
+		onInit() {
+			let oModel = new ODataModel("http://localhost:4000/odata", {
+                defaultBindingMode: "TwoWay",
+                useBatch: false,
+                headers: {
+                    "Content-Type": "application/atom+xml",
+                },
+                json: false,
+                maxDataServiceVersion: "3.0"
+            } 
+			); this.getView().setModel(oModel)}
+			
 	});
 });
